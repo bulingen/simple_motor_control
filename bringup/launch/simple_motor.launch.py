c930c9351521aaceb8eaa6cee3b21fc2f24aee2e
bulingen <adamlecorney@gmail.com>
2025-07-14 17:55:40 +0200

    feat: add more boilerplate stuff. not really working

--- bringup/launch/simple_motor.launch.py
+++ bringup/launch/simple_motor.launch.py
@@ -1,12 +1,34 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    package_name = "simple_motor_control"
+    
+    controller_config = PathJoinSubstitution([
+        FindPackageShare("simple_motor_control"),
+        "config",
+        "simple_motor_controllers.yaml"
+    ])
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(package_name), "urdf", "simple_motor.urdf.xacro"]
+            )
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
     return LaunchDescription([
         Node(
             package="controller_manager",
             executable="ros2_control_node",
-            parameters=[],
+            parameters=[robot_description, controller_config],
             output="screen"
         )
     ])
